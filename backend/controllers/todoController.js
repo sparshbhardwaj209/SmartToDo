3fd4647a59aac255f11cbd89377270fc1d924887
Sparsh Bhardwaj <sparshbhardwaj209@gmail.com>
2025-02-14 17:04:59 +0530

    create new todo api added

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -1,11 +1,28 @@
 const Todo = require("../models/Todo");
 
+// get all todos
 exports.getTodos = async (req, res) => {
   try {
     const todos = await Todo.find({ user: req.user.id }).sort({
       createdAt: -1,
     });
     res.json(todos);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+// create a todo api
+exports.createTodo = async (req, res) => {
+  const { text } = req.body;
+  try {
+    const newTodo = new Todo({
+      text,
+      user: req.user.id,
+    });
+    await newTodo.save();
+    res.status(201).json(newTodo);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error" });
--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -4,5 +4,6 @@ const { getTodos } = require("../controllers/todoController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/", authMiddleware, getTodos);
+router.post("/, authMiddleware, createTodo");
 
 module.exports = router;
