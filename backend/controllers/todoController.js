7baef8d1aa2188491898981042d274c841ed7f50
Sparsh Bhardwaj <sparshbhardwaj209@gmail.com>
2025-02-14 17:11:27 +0530

    created updateTodo api

--- backend/controllers/todoController.js
+++ backend/controllers/todoController.js
@@ -28,3 +28,26 @@ exports.createTodo = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+// update a todo api
+exports.updateTodo = async (req, res) => {
+  const { id } = req.params;
+  const { completed, text } = req.body;
+  try {
+    const todo = await Todo.findOneAndUpdate(
+      {
+        _id: id,
+        user: req.user.id,
+      },
+      { completed, text },
+      { new: true }
+    );
+    if (!todo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+    res.json(todo);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getTodos } = require("../controllers/todoController");
+const {
+  getTodos,
+  createTodo,
+  updateTodo,
+} = require("../controllers/todoController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/", authMiddleware, getTodos);
-router.post("/, authMiddleware, createTodo");
+
+router.post("/", authMiddleware, createTodo);
+
+router.put("/:id", authMiddleware, updateTodo);
 
 module.exports = router;
