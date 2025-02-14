179b53d784d51e3aa9cd8d74efe2e2f001970baa
Sparsh Bhardwaj <sparshbhardwaj209@gmail.com>
2025-02-14 14:32:52 +0530

    Created Auth Router for user registration

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,6 +5,7 @@ exports.register = async (req, res) => {
   try {
     let user = new User({ email, password });
     await user.save();
+    res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
     console.error(err.message);
     res.status(500).json({ message: "Server Error" });
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const connectDB = require("./config/db");
+require("dotenv").config();
+const authRoutes = require("./routes/auth.js");
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
