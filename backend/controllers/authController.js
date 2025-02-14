81c78f58fb6baea21782cb89213d198bb48e730b
Sparsh Bhardwaj <sparshbhardwaj209@gmail.com>
2025-02-14 14:39:01 +0530

    hashed user password before saving them to the database

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,9 +1,24 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 exports.register = async (req, res) => {
   const { email, password } = req.body;
   try {
-    let user = new User({ email, password });
+    // checking if the user exist or not
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // hashing the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // creating new instance of the user
+    user = new User({
+      email,
+      password: hashedPassword,
+    });
     await user.save();
     res.status(201).json({ message: "User registered successfully" });
   } catch (err) {
