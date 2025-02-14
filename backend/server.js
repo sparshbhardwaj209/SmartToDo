1182fe5e2b799f7548922076871c5e01698d0f30
Sparsh Bhardwaj <sparshbhardwaj209@gmail.com>
2025-02-14 15:04:18 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 require("dotenv").config();
 const authRoutes = require("./routes/auth.js");
+const todoRoutes = require("./routes/todo.js");
 const PORT = process.env.PORT || 5000;
 
 connectDB();
@@ -12,6 +13,6 @@ app.use(express.json());
 
 // routes
 app.use("/api/auth", authRoutes);
-
+app.use("/api/todos", todoRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
